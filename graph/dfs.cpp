8526714c9fdb130e68382bd48999d7d324008b41
rruan16 <rodrigoruan16@hotmail.com>
2025-03-28 15:43:04 -0300

    DFS

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -1,11 +1,50 @@
-void dfs(int v) {
-    if (visited[v])
-        return;
+#include <bits/stdc++.h>
+ 
+using namespace std;
 
-    visited[v] = 1;
+vector<vector<int>> G;
+vector<int> visited;
 
-    for (int c : G[v]) {
-        if (!visited[c])
-            dfs(c);
+void dfs(int city) {
+    stack<int> st;
+    st.push(city);
+
+    while (!st.empty()) {
+        int c = st.top();
+        st.pop();
+
+        if (visited[c])
+            continue;
+
+        visited[c] = 1;
+
+        for (int p : G[c]) {
+            if (!visited[p])
+                st.push(p);
+        }
     }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    G = vector<vector<int>>(n + 1, vector<int>());
+
+    int a, b;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+
+    int total = 0;
+
+    string ans;
+    visited = vector<int>(n + 1, 0);
+
+    return 0;
 }
---
+++ graph/djikstra.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+vector<vector<pair<int, int>>> G;
+vector<int> visited;
+vector<int> dist;
+
+void dijkstra(int i) {
+    priority_queue<pair<int, int>> q;
+    dist[i] = 0;
+    q.push({0, i});
+
+    while (!q.empty()) {
+        int a = q.top().second;
+        q.pop();
+
+        if (visited[a])
+            continue;
+        
+        visited[a] = 1;
+
+        for (pair<int, int> p : G[a]) {
+            int b = p.first, w = p.second;
+            if (dist[a] + w < dist[b]) {
+                dist[b] = dist[a] + w;
+                q.push({ -dist[b], b });
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout << fixed << setprecision(0); // remove when working with floating point
+
+    int n, m;
+    cin >> n >> m;
+
+    G = vector<vector<pair<int, int>>> (n + 2, vector<pair<int ,int>>());
+    visited = vector<int> (n + 2, 0);
+    dist = vector<int> (n + 2, INFINITY);
+
+    int s, t, b;
+    while (m--) {
+        cin >> s >> t >> b;
+        G[s].push_back({t, b});
+        G[t].push_back({s, b});
+    }
+
+    dijkstra(0);
+
+    cout << dist[n + 1] << '\n';
+
+    return 0;
+}
