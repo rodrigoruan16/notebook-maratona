7d64cc29a6a51e8d29bc7749f6735c410dca6b17
rruan16 <rodrigoruan16@hotmail.com>
2025-03-28 15:43:56 -0300

    DFS GRID

---
+++ graph/dfsGrid.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+vector<vector<char>> G;
+vector<vector<int>> visited;
+vector<pair<int, int>> moves = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+
+void dfs(int i, int j) {
+    if (visited[i][j])
+        return;
+
+    visited[i][j] = 1;
+
+    for (pair<int, int> p : moves) {
+        int I = i + p.first, J = j + p.second;
+
+        if (I < 0 || J < 0 || I >= G.size() || J >= G[0].size())
+            continue;
+
+        dfs(I, J);
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    G = vector<vector<char>>(n, vector<char>(m));
+    visited =vector<vector<int>>(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> G[i][j];
+        }
+    }
+
+    return 0;
+}
