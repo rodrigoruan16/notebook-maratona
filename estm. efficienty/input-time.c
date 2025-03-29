975320153fae81e2bef1a87e8dd7432b43b4b035
rruan16 <rodrigoruan16@hotmail.com>
2025-03-29 15:03:19 -0300

    .

--- estm. efficienty/input-time.c
+++ estm. efficienty/input-time.c
@@ -1,7 +1,7 @@
 input size       required time complexity
 n <= 10                 O(n!)
 n <= 20                 O(2^n)
-n <= 500                O(n³)
-n <= 5000               O(n²)
-n <= 10⁶              O(n log n) or O(n)
+n <= 500                O(n^3)
+n <= 5000               O(n^2)
+n <= 10^6              O(n log n) or O(n)
 n is large              O(1) or O(log n)
