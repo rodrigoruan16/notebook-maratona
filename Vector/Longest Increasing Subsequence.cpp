d2b5787d4acaaeb749d04b0537e2b9084ced8d80
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-07-10 19:48:14 -0300

    Update lis

--- Vector/Longest Increasing Subsequence.cpp
+++ Vector/Longest Increasing Subsequence.cpp
@@ -1,88 +1,48 @@
-/*
-Given an array arr[] of size N, the task is to find the length of the Longest Increasing Subsequence (LIS) i.e., the longest possible subsequence in which the elements of the subsequence are sorted in increasing order, in O(N log N).
-
-Examples:            
-
-Input: arr[] = {3, 10, 2, 1, 20}
-Output: 3
-Explanation: The longest increasing subsequence is 3, 10, 20
-
-
-Input: arr[] = {3, 2}
-Output:1
-Explanation: The longest increasing subsequences are {3} and {2}
-
-
-Input: arr[] = {50, 3, 10, 7, 40, 80}
-Output: 4
-Explanation: The longest increasing subsequence is {3, 7, 40, 80}
-*/
-
-// Binary Search Approach of Finding LIS by
-// reducing the problem to longest
-// common Subsequence
 #include <bits/stdc++.h>
 using namespace std;
 
-int lengthOfLIS(vector<int>& arr)
-{
-
-    // Binary search approach
-    int n = arr.size();
-    vector<int> ans;
-
-    // Initialize the answer vector with the
-    // first element of arr
-    ans.push_back(arr[0]);
-
-    for (int i = 1; i < n; i++) {
-        if (arr[i] > ans.back()) {
-
-            // If the current number is greater
-            // than the last element of the answer
-            // vector, it means we have found a
-            // longer increasing subsequence.
-            // Hence, we append the current number
-            // to the answer vector.
-            ans.push_back(arr[i]);
-        }
-        else {
-
-            // If the current number is not
-            // greater than the last element of
-            // the answer vector, we perform
-            // a binary search to find the smallest
-            // element in the answer vector that
-            // is greater than or equal to the
-            // current number.
-
-            // The lower_bound function returns
-            // an iterator pointing to the first
-            // element that is not less than
-            // the current number.
-            int low = lower_bound(ans.begin(), ans.end(),
-                                  arr[i])
-                      - ans.begin();
-
-            // We update the element at the
-            // found position with the current number.
-            // By doing this, we are maintaining
-            // a sorted order in the answer vector.
-            ans[low] = arr[i];
+int lis(vector<int> const& a) {
+    int n = a.size();
+    vector<int> d(n, 1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i])
+                d[i] = max(d[i], d[j] + 1);
         }
     }
 
-    // The length of the answer vector
-    // represents the length of the
-    // longest increasing subsequence.
-    return ans.size();
+    int ans = d[0];
+    for (int i = 1; i < n; i++) {
+        ans = max(ans, d[i]);
+    }
+    return ans;
 }
 
-// Driver program to test above function
-int main()
-{
-    vector<int> arr = { 10, 22, 9, 33, 21, 50, 41, 60 };
-    // Function call
-    printf("Length of LIS is %d\n", lengthOfLIS(arr));
-    return 0;
+vector<int> lisRestoring(vector<int> const& a) {
+    int n = a.size();
+    vector<int> d(n, 1), p(n, -1);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i] && d[i] < d[j] + 1) {
+                d[i] = d[j] + 1;
+                p[i] = j;
+            }
+        }
+    }
+
+    int ans = d[0], pos = 0;
+    for (int i = 1; i < n; i++) {
+        if (d[i] > ans) {
+            ans = d[i];
+            pos = i;
+        }
+    }
+
+    vector<int> subseq;
+    while (pos != -1) {
+        subseq.push_back(a[pos]);
+        pos = p[pos];
+    }
+    reverse(subseq.begin(), subseq.end());
+    return subseq;
 }
