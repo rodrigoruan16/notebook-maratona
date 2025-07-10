b20e47a4c6988e53c3b730d5bd1fabf577ddd8a0
rodrigoruan16 <rodrigoruan16@hotmail.com>
2025-07-10 19:51:02 -0300

    Update LIS

--- Vector/Longest Increasing Subsequence.cpp
+++ Vector/Longest Increasing Subsequence.cpp
@@ -1,23 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int lis(vector<int> const& a) {
+// O(n log n)
+int lis(string& a) {
     int n = a.size();
-    vector<int> d(n, 1);
+    const int INF = 1e9;
+    vector<int> d(n+1, INF);
+    d[0] = -INF;
+
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i; j++) {
-            if (a[j] < a[i])
-                d[i] = max(d[i], d[j] + 1);
-        }
+        int l = upper_bound(d.begin(), d.end(), a[i]) - d.begin();
+        if (d[l-1] <= a[i] && a[i] < d[l])
+            d[l] = a[i];
     }
 
-    int ans = d[0];
-    for (int i = 1; i < n; i++) {
-        ans = max(ans, d[i]);
+    int ans = 0;
+    for (int l = 0; l <= n; l++) {
+        if (d[l] < INF)
+            ans = l;
     }
     return ans;
 }
 
+// O(n²)
 vector<int> lisRestoring(vector<int> const& a) {
     int n = a.size();
     vector<int> d(n, 1), p(n, -1);
